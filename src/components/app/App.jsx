7024d19d4e55abc3d6725aee6fb3919af3abe084
Alexandr Gaytsuk <gayrsuk@gmail.com>
2021-06-25 14:34:01 +0300

    fetching data

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './app.module.scss';
 
 const App = () => {
+    const storage = window.localStorage;
+    const [ state, setState ] = useState([]);
+
+
+    const toFetch = () => {
+        fetch('https://yalantis-react-school-api.yalantis.com/api/task0/users')
+            .then(data => data.text()
+            )
+            .then(data => setState(JSON.parse(data)));
+    };
+
+
+    if (!storage.getItem('employees')) {
+        console.log('bingo');
+        toFetch();
+        storage.setItem('employees', JSON.stringify(state))
+    }
+
+    console.log(JSON.parse(storage.getItem('employees')));
+
+    console.log('state => ', state);
+
     return (
         <div className={classes.app}>
 
